a3a388c3bb041b3e08e230ebf6d3fd4a0284657c
elijahjimenez13 <107581688+elijahjimenez13@users.noreply.github.com>
2023-07-11 13:11:38 -0700

    FInished Clicky Clicky Unit 5

---
+++ Clicky Clicky Unit 5/Assets/Scripts/Target.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private float minSpeed = 12.0f;
+    private float maxSpeed = 16.0f;
+    private float maxTorque = 10.0f;
+    private float xRange = 4.0f;
+    private float ySpawnPos = -3.0f;
+
+    public int pointValue;
+    
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    public ParticleSystem explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(),RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed,maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque,maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange,xRange), ySpawnPos, 0);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider) 
+    {
+        Destroy(gameObject);
+        
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+}
