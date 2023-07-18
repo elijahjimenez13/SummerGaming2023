ad8821cce029fd4e462bcc6abaac339e5fccd7e6
elijahjimenez13 <107581688+elijahjimenez13@users.noreply.github.com>
2023-07-18 14:16:09 -0700

    Implemented Portals
    - Created a Portal prefab to use throughout scenes
    - The portal prefab includes  rigid body, box collider, and a portal script that uses numerator function that changes the scene
    - Implemented a 2nd Dark Swampy scene for a pathway
    - Implemented a spawn point for the player to to know where they will be once they go through a portal
    - Fixed error with moving during cutscene and fixed it by leaving only 1 player controller component on the player

---
+++ Legends Awaken/Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Controls;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        GameObject player;
+        PlayableDirector director;
+
+        private void Awake()
+        {
+            player = GameObject.FindWithTag("Player");
+            director = GetComponent<PlayableDirector>();
+        }
+
+        private void OnEnable()
+        {
+            director.played += DisableControl;
+            director.stopped += EnableControl;
+        }
+
+        private void OnDisable()
+        {
+            director.played -= DisableControl;
+            director.stopped -= EnableControl;
+        }
+
+        void DisableControl(PlayableDirector pd)
+        {
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+}
