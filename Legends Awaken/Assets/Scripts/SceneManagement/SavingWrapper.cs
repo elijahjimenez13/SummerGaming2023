a68241703df5ce672495d752a508cb5e192be987
elijahjimenez13 <107581688+elijahjimenez13@users.noreply.github.com>
2023-09-10 19:38:52 -0700

    Complete Pre-Celebration Project
    - Honestly can't name all changes because it was done over time but there was final touches added -->
    - Specific drops for knights, mages, archers, and boss
    - New locations for healing pumpkins
    - New main menu keybind text

--- Legends Awaken/Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Legends Awaken/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using RPG.Saving;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,20 +9,48 @@ namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
-        const string defaultSaveFile = "save";
-
+        private const string currentSaveKey = "currentSaveName";
         [SerializeField] float fadeInTime = 0.2f;
+        [SerializeField] float fadeOutTime = 0.2f;
+        [SerializeField] int firstLevelBuildIndex = 1;
+        [SerializeField] int menuLevelBuildIndex = 0;
 
-        private void Awake()
+        public void LoadMenu()
         {
-            StartCoroutine(LoadLastScene());
+            StartCoroutine(LoadMenuScene());
+        }
+
+        public void SetCurrentSave(string saveFile)
+        {
+            PlayerPrefs.SetString(currentSaveKey, saveFile);
+        }
+
+        private string GetCurrentSave()
+        {
+            return PlayerPrefs.GetString(currentSaveKey);
         }
 
         private IEnumerator LoadLastScene()
         {
-            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
             Fader fader = FindObjectOfType<Fader>();
-            fader.FadeOutImmediate();
+            yield return fader.FadeOut(fadeOutTime);
+            yield return GetComponent<SavingSystem>().LoadLastScene(GetCurrentSave());
+            yield return fader.FadeIn(fadeInTime);
+        }
+
+        private IEnumerator LoadFirstScene()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(firstLevelBuildIndex);
+            yield return fader.FadeIn(fadeInTime);
+        }
+
+        private IEnumerator LoadMenuScene()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(menuLevelBuildIndex);
             yield return fader.FadeIn(fadeInTime);
         }
 
@@ -42,22 +72,22 @@ namespace RPG.SceneManagement
 
         public void Load()
         {
-            StartCoroutine(GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile));
+            GetComponent<SavingSystem>().Load(GetCurrentSave());
         }
 
         public void Save()
         {
-            GetComponent<SavingSystem>().Save(defaultSaveFile);
+            GetComponent<SavingSystem>().Save(GetCurrentSave());
         }
 
         public void Delete()
         {
-            GetComponent<SavingSystem>().Delete(defaultSaveFile);
+            GetComponent<SavingSystem>().Delete(GetCurrentSave());
         }
 
         public void ContinueGame()
         {
-            SceneManager.LoadScene(1);
+            StartCoroutine(LoadLastScene());
         }
 
         public void NewGame(string saveFile)
